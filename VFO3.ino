01603bb9366b2f527e3da4f02547d857a2049396
Bruce MacKinnon <bruce@mackinnon.com>
2019-02-24 15:06:05 -0500

    Added ability to switch between high/low side injection

--- VFO3.ino
+++ VFO3.ino
@@ -15,7 +15,7 @@
 #include <ClickDetector.h>
 #include <Utils.h>
 
-#define MAGIC_NUMBER 2727
+#define MAGIC_NUMBER 2728
 
 Adafruit_SSD1306 display(128,64);
 Si5351 si5351;
@@ -34,20 +34,43 @@ Si5351 si5351;
 // Analog sample input
 #define DETECTOR_INPUT_PIN A0
 
-enum Mode { VFO, VFO_OFFSET, BFO, CAL, VFO_POWER, BFO_POWER, METER, MODE };
-const char* modeTitles[] = { "VFO", "VFO+", "BFO", "CAL", "VFOPwr", "BFOPwr", "Meter", "Mode" };
-Mode mode = VFO;
+// ----------------------------------------------------------------------------------------
+// Menu choices
+enum Mode { VFO, MODE, BPF_TOP, BPF_WIDTH, CAL, LO_POWER, BFO_POWER, METER1, LO_SIDE };
+const char* ModeTitles[] = { "VFO", "Mode", "BPFTop", "BPFWide", "CAL", "LOPwr", "BFOPwr", "Meter1", "LOSide" };
+const int ModeChoices = 9;
 
-const unsigned long stepMenu[] = { 500, 100, 10, 1, 1000000, 100000, 10000, 1000 };
-const char* stepMenuTitles[] = { "500 Hz", "100 Hz", "10 Hz", "1 Hz", "1 MHz", "100 kHz", "10 kHz", "1 kHz" };
-const uint8_t maxStepIndex = 7;
+const unsigned long Step[] = { 500, 100, 10, 1, 1000000, 100000, 10000, 1000 };
+const char* StepTitles[] = { "500 Hz", "100 Hz", "10 Hz", "1 Hz", "1 MHz", "100 kHz", "10 kHz", "1 kHz" };
+const int StepChoices = 8;
 
 enum Mode2 { LSB, USB };
-Mode2 mode2 = LSB;
+const char* Mode2Titles[] = { "LSB", "USB" };
+const int Mode2Choices = 2;
+
+enum LOSide { LOWSIDE_INJECTION, HIGHSIDE_INJECTION, DIRECT_CONVERSION };
+const char* LOSideTitles[] = { "Low Side", "High Side", "Direct" };
+const int LOSideChoices = 3;
+
+const int LOPowerChoices = 4;
+const int BFOPowerChoices = 4;
 
 // 40m band limitations used for scanning (phone portion only)
-const unsigned long minDisplayFreq = 7125000L;
-const unsigned long maxDisplayFreq = 7300000L;
+const unsigned long MinDisplayFreq = 7125000L;
+const unsigned long MaxDisplayFreq = 7300000L;
+
+// The actual settings for the rig
+Mode ModeSetting = VFO;
+int StepSetting = 0;
+Mode2 Mode2Setting = LSB;
+LOSide LOSideSetting = LOWSIDE_INJECTION;
+uint8_t BFOPowerSetting = 0;
+uint8_t LOPowerSetting = 0;
+unsigned long DisplayFreq = 7000000L;
+unsigned long BPFTopFreq = 11998000l;
+unsigned long BPFWidth = 4000L; 
+long CalPpm = 0;
+int AnalogSample1 = 0;
 
 DebouncedSwitch2 db2(5L);
 DebouncedSwitch2 db3(5L);
@@ -58,34 +81,14 @@ DebouncedSwitch2 pttButton(20L);
 RotaryEncoder renc(&db2,&db3,100L);
 ClickDetector cd4(&db4);
 
-unsigned long vfoFreq = 7000000;
-long vfoOffsetFreq = 11998000;
-unsigned long bfoFreq = 11998000;
-long calPpm = 0;
-uint8_t stepIndex = 0;
-uint8_t vfoPower = 0;
-uint8_t bfoPower = 0;
-int analogSample = 0;
-
-// Scanning related.
+// Scanning related
 bool scanMode = false;
 // This is the last time we made a scan jump
 unsigned long lastScanStamp = 0;
 // This controls how fast we scan
 unsigned long scanDelayMs = 150;
 
-// Create an IntervalTimer object 
-IntervalTimer myTimer;
-
-int outTable[16];
-int outTablePtr = 0;
-
-void audioCycle() {
-  analogWrite(A14,outTable[outTablePtr++]);
-  if (outTablePtr == 16) {
-    outTablePtr = 0;  
-  }
-}
+int Fault = 0;
 
 unsigned long getMH(unsigned long f) {
   return f / 1000000L;
@@ -104,6 +107,7 @@ void updateDisplay1() {
   int startX = 10;
   int y = 17;
   char buf[4];
+  
   // Render frequency
   unsigned long f = 0;
   boolean neg = false;  
@@ -111,8 +115,8 @@ void updateDisplay1() {
   display.setTextSize(2);
   display.setTextColor(WHITE);
   
-  if (mode == VFO) {
-    f = vfoFreq;
+  if (ModeSetting == VFO) {
+    f = DisplayFreq;
     display.setCursor(startX,y);
     display.print(getMH(f)); 
     display.setCursor(startX + 30,y);
@@ -124,9 +128,9 @@ void updateDisplay1() {
     // Step
     display.setTextSize(0);
     display.setCursor(startX,55);
-    display.print(stepMenuTitles[stepIndex]);
-  } else if (mode == VFO_OFFSET) {
-    f = vfoOffsetFreq;
+    display.print(StepTitles[StepSetting]);
+  } else if (ModeSetting == BPF_TOP) {
+    f = BPFTopFreq;
     display.setCursor(startX,y);
     display.print(getMH(f)); 
     display.setCursor(startX + 30,y);
@@ -138,24 +142,18 @@ void updateDisplay1() {
     // Step
     display.setTextSize(0);
     display.setCursor(startX,55);
-    display.print(stepMenuTitles[stepIndex]);
-  } else if (mode == BFO) {
-    f = bfoFreq;
+    display.print(StepTitles[StepSetting]);
+  } else if (ModeSetting == BPF_WIDTH) {
+    f = BPFWidth;
     display.setCursor(startX,y);
-    display.print(getMH(f)); 
-    display.setCursor(startX + 30,y);
-    sprintf(buf,"%03lu",getKH(f));
-    display.print(buf);
-    display.setCursor(startX + 70,y);
-    sprintf(buf,"%03lu",getH(f));
-    display.print(buf);
+    display.print(f); 
     // Step
     display.setTextSize(0);
     display.setCursor(startX,55);
-    display.print(stepMenuTitles[stepIndex]);
-  } else if (mode == CAL) {
-    f = abs(calPpm);
-    neg = (calPpm < 0);
+    display.print(StepTitles[StepSetting]);
+  } else if (ModeSetting == CAL) {
+    f = abs(CalPpm);
+    neg = (CalPpm < 0);
     // Sign
     if (neg) {
       display.drawLine(0,y+6,5,y+6,1);
@@ -165,26 +163,25 @@ void updateDisplay1() {
     // Step
     display.setTextSize(0);
     display.setCursor(startX,55);
-    display.print(stepMenuTitles[stepIndex]);
-  } else if (mode == VFO_POWER) {
-    f = vfoPower;
+    display.print(StepTitles[StepSetting]);
+  } else if (ModeSetting == LO_POWER) {
+    f = LOPowerSetting;
     display.setCursor(startX,y);
     display.print(f); 
-  } else if (mode == BFO_POWER) {
-    f = bfoPower;
+  } else if (ModeSetting == BFO_POWER) {
+    f = BFOPowerSetting;
     display.setCursor(startX,y);
     display.print(f); 
-  } else if (mode == METER) {
-    f = analogSample;
+  } else if (ModeSetting == METER1) {
+    f = AnalogSample1;
     display.setCursor(startX,y);
     display.print(f); 
-  } else if (mode == MODE) {
+  } else if (ModeSetting == MODE) {
     display.setCursor(startX,y);
-    if (mode2 == LSB) {
-      display.print("LSB"); 
-    } else if (mode2 == USB) {
-      display.print("USB");       
-    }
+    display.print(Mode2Titles[Mode2Setting]); 
+  } else if (ModeSetting == LO_SIDE) {
+    display.setCursor(startX,y);
+    display.print(LOSideTitles[LOSideSetting]); 
   }
 }
 
@@ -194,46 +191,111 @@ void updateDisplay() {
   display.setCursor(0,0);
   display.setTextSize(0);
   display.setTextColor(WHITE);
-  display.println("KC1FSZ VFO3");
+  display.println("KC1FSZ VFO3.1");
   display.drawLine(0,15,display.width(),15,WHITE);
-  
-  // Mode
-  int modeX = 85;
-  display.setCursor(modeX,0);
-  display.print(modeTitles[(int)mode]);
 
-  updateDisplay1();
-}
-
-/**
- * Called to load frequencies into the Si5351
- */
-void updateVFOFreq() {
-  long f;
-  if (mode2 == LSB) {
-    f = vfoOffsetFreq - vfoFreq;
-    si5351.set_freq((unsigned long long)f * 100ULL,SI5351_CLK0);
-  } else if (mode2 == USB) {
-    f = vfoOffsetFreq + vfoFreq;
-    si5351.set_freq((unsigned long long)f * 100ULL,SI5351_CLK0);    
+  if (Fault) {
+    display.setTextSize(2);
+    display.setCursor(0,10);
+    display.print(Fault);
+  } else {
+    // Mode at the top of the screen
+    int modeX = 85;
+    display.setCursor(modeX,0);
+    display.print(ModeTitles[(int)ModeSetting]);
+    // Mode specific information 
+    updateDisplay1();
   }
 }
 
+// Called to load the LO frequency into the Si5351 (CLK0)
+void updateLOFreq() {
+  long f = 0;
+  if (LOSideSetting == LOWSIDE_INJECTION) {
+    if (Mode2Setting == LSB) {
+      f = BPFTopFreq - DisplayFreq;
+    } else if (Mode2Setting == USB) {
+      f = (BPFTopFreq - DisplayFreq) - BPFWidth;
+    }
+  } else if (LOSideSetting == HIGHSIDE_INJECTION) {
+    if (Mode2Setting == USB) {
+      f = BPFTopFreq + DisplayFreq;
+    } else if (Mode2Setting == LSB) {
+      f = (BPFTopFreq + DisplayFreq) - BPFWidth;
+    }
+  } else if (LOSideSetting == DIRECT_CONVERSION) {
+    f = DisplayFreq;
+  } else {
+    Fault = 1;
+  }
+  si5351.set_freq((unsigned long long)f * 100ULL,SI5351_CLK0);    
+}
+
+// Called to load the BFO frequency into the Si5351 (CLK2)
 void updateBFOFreq() {
-  long f = bfoFreq;
+  long f = 0;
+  if (LOSideSetting == LOWSIDE_INJECTION) {
+    if (Mode2Setting == LSB) {
+      f = BPFTopFreq;
+    } else if (Mode2Setting == USB) {
+      f = BPFTopFreq - BPFWidth;
+    }
+  } else if (LOSideSetting == HIGHSIDE_INJECTION) {
+    if (Mode2Setting == USB) {
+      f = BPFTopFreq;
+    } else if (Mode2Setting == LSB) {
+      f = BPFTopFreq - BPFWidth;
+    }
+  } else if (LOSideSetting == DIRECT_CONVERSION) {
+    f = DisplayFreq;
+  } else {
+    Fault = 1;
+  }
   si5351.set_freq((unsigned long long)f * 100ULL,SI5351_CLK2);
 }
 
-void updateVFOPower() {
-  si5351.drive_strength(SI5351_CLK0,(si5351_drive)vfoPower);
+void updateLOPower() {
+  si5351.drive_strength(SI5351_CLK0,(si5351_drive)LOPowerSetting);
 }
 
 void updateBFOPower() {
-  si5351.drive_strength(SI5351_CLK2,(si5351_drive)bfoPower);
+  si5351.drive_strength(SI5351_CLK2,(si5351_drive)BFOPowerSetting);
 }
 
 void updateCal() {
-  si5351.set_correction(calPpm,SI5351_PLL_INPUT_XO);
+  si5351.set_correction(CalPpm,SI5351_PLL_INPUT_XO);
+}
+
+void loadState() {
+  // Pull values from EEPROM
+  long magic = Utils::eepromReadLong(0);
+  // Check to make sure that we have valid information in the EEPROM.  For instance,
+  // if this is a new processor we might not have saved anything. 
+  if (magic != MAGIC_NUMBER) {
+    return;
+  }
+  DisplayFreq = Utils::eepromReadLong(4);
+  BPFTopFreq = Utils::eepromReadLong(8);
+  BPFWidth = Utils::eepromReadLong(12);
+  CalPpm = Utils::eepromReadLong(16);
+  StepSetting = EEPROM.read(20);   
+  Mode2Setting = (Mode2)EEPROM.read(21);
+  LOSideSetting =  (LOSide)EEPROM.read(22);
+  LOPowerSetting = EEPROM.read(23);
+  BFOPowerSetting = EEPROM.read(24);
+}
+
+void saveState() {
+  Utils::eepromWriteLong(0,MAGIC_NUMBER);
+  Utils::eepromWriteLong(4,DisplayFreq);
+  Utils::eepromWriteLong(8,BPFTopFreq);
+  Utils::eepromWriteLong(12,BPFWidth);
+  Utils::eepromWriteLong(16,CalPpm);
+  EEPROM.write(20,StepSetting);
+  EEPROM.write(21,Mode2Setting);
+  EEPROM.write(22,LOSideSetting);
+  EEPROM.write(23,LOPowerSetting);  
+  EEPROM.write(24,BFOPowerSetting);  
 }
 
 void setup() {
@@ -274,34 +336,21 @@ void setup() {
   display.clearDisplay();
 
   // Pull values from EEPROM
-  long magic = Utils::eepromReadLong(0);
-  // Check to make sure that we have valid information in the EEPROM.  For instance,
-  // if this is a new processor we might not have saved anything. 
-  if (magic == MAGIC_NUMBER) {
-    vfoFreq = Utils::eepromReadLong(4);
-    vfoOffsetFreq = Utils::eepromReadLong(8);
-    bfoFreq = Utils::eepromReadLong(12);
-    calPpm = Utils::eepromReadLong(16);
-    stepIndex = EEPROM.read(20);    
-    vfoPower = EEPROM.read(21);
-    bfoPower = EEPROM.read(22);
-  } 
+  loadState();
   
-  if (stepIndex > maxStepIndex) {
-    stepIndex = 0;
-  }
-
   // Initial update of Si5351
-  updateVFOFreq();
+  updateLOFreq();
   updateBFOFreq();
-  updateVFOPower();
+  updateLOPower();
   updateBFOPower();
   updateCal();
 
   // Initial display render
   updateDisplay();
+  
   display.display();
 
+  /*
   for (int i = 0; i < 16; i++) {
     float rad = 2.0f * 3.14159f * ((float)i / 16.0f);
     float a = sin(rad);
@@ -309,9 +358,9 @@ void setup() {
     float c = (b / 2.0f) * 1024.0f;
     outTable[i] = (int)c;
   }
-
   // Audio cycle 
   myTimer.begin(audioCycle,200);
+  */
 }
 
 int count = 0;
@@ -319,9 +368,13 @@ int count = 0;
 void loop() {
     
   boolean displayDirty = false;
+  
+  if (Fault) {
+    displayDirty = true;
+  }
+  
   int c2 = digitalRead(PIN_D2);
   int c3 = digitalRead(PIN_D3);
-
   db2.loadSample(c2 == 0);
   db3.loadSample(c3 == 0);
   db4.loadSample(digitalRead(PIN_D4) == 0);
@@ -332,90 +385,63 @@ void loop() {
   long mult = renc.getIncrement();
   long clickDuration = cd4.getClickDuration();
 
+  // Sample analog for meter 1
   int sample = analogRead(DETECTOR_INPUT_PIN);
-  
-  if (sample != analogSample && mode == METER) {
+  if (sample != AnalogSample1 && ModeSetting == METER1) {
     displayDirty = true;
   }
-  analogSample = sample;
+  AnalogSample1 = sample;
 
-  // Look for dial turning
+  // Look for dial turning action
   if (mult != 0) {
     // Immediately stop scanning
     scanMode = false;
     // Handle dial
-    long step = mult * stepMenu[stepIndex];
-    if (mode == VFO) {
-      vfoFreq += step;
-      updateVFOFreq();
-    } else if (mode == VFO_OFFSET) {
-      vfoOffsetFreq += step;
-      updateVFOFreq();
-    } else if (mode == BFO) {
-      bfoFreq += step;
+    long step = mult * Step[StepSetting];
+    if (ModeSetting == VFO) {
+      DisplayFreq += step;
+      updateLOFreq();
+    } else if (ModeSetting == BPF_TOP) {
+      BPFTopFreq += step;
+      updateLOFreq();
       updateBFOFreq();
-    } else if (mode == CAL) {
-      calPpm += step;
+    } else if (ModeSetting == BPF_WIDTH) {
+      BPFWidth += step;
+      updateLOFreq();
+      updateBFOFreq();
+    } else if (ModeSetting == CAL) {
+      CalPpm += step;
       updateCal();
-    } else if (mode == VFO_POWER) {
-      vfoPower += 1;
-      if (vfoPower > 3) {
-        vfoPower = 0;
-      }
-      updateVFOPower();     
-    } else if (mode == BFO_POWER) {
-      bfoPower += 1;
-      if (bfoPower > 3) {
-        bfoPower = 0;
-      }
+    } else if (ModeSetting == LO_POWER) {
+      LOPowerSetting = (LOPowerSetting == LOPowerChoices - 1) ? 0 : LOPowerSetting + 1;
+      updateLOPower();     
+    } else if (ModeSetting == BFO_POWER) {
+      BFOPowerSetting = (BFOPowerSetting == BFOPowerChoices - 1) ? 0 : BFOPowerSetting + 1;
       updateBFOPower();     
-    } else if (mode == MODE) {
-      if (mode2 == LSB) {
-        mode2 = USB;
-      } else if (mode2 == USB) {
-        mode2 = LSB;
-      }
+    } else if (ModeSetting == MODE) {
+      Mode2Setting = (Mode2Setting == Mode2Choices - 1) ? (Mode2)0 : (Mode2)(Mode2Setting + 1);
+      updateLOFreq();
+      updateBFOFreq();      
+    } else if (ModeSetting == LO_SIDE) {
+      LOSideSetting = (LOSideSetting == LOSideChoices - 1) ? (LOSide)0 : (LOSide)(LOSideSetting + 1);
+      updateLOFreq();
+      updateBFOFreq();
     }
-
     displayDirty = true;
   }
   // Save frequencies in EEPROM
   else if (clickDuration > 5000) {   
-    Utils::eepromWriteLong(0,MAGIC_NUMBER);
-    Utils::eepromWriteLong(4,vfoFreq);
-    Utils::eepromWriteLong(8,vfoOffsetFreq);
-    Utils::eepromWriteLong(12,bfoFreq);
-    Utils::eepromWriteLong(16,calPpm);
-    EEPROM.write(20,stepIndex);
-    EEPROM.write(21,vfoPower);
-    EEPROM.write(22,bfoPower);
+    saveState();
   } else if (clickDuration > 0) {
-    if (++stepIndex > maxStepIndex) {
-      stepIndex = 0;
-    } 
+    StepSetting = (StepSetting == StepChoices - 1) ? 0 : StepSetting + 1;
     displayDirty = true;
   } else if (commandButton1.getState() && commandButton1.isEdge()) {
-    if (mode == VFO) {
+    if (ModeSetting == VFO) {
       scanMode = !scanMode;     
     }
+    displayDirty = true;
   } else if (commandButton2.getState() && commandButton2.isEdge()) {
-    if (mode == VFO) {
-      mode = VFO_OFFSET;
-    } else if (mode == VFO_OFFSET) {
-      mode = BFO;
-    } else if (mode == BFO) {
-      mode = CAL;
-    } else if (mode == CAL) {
-      mode = VFO_POWER;
-    } else if (mode == VFO_POWER) {
-      mode = BFO_POWER;
-    } else if (mode == BFO_POWER) {
-      mode = METER;
-    } else if (mode == METER) {
-      mode = MODE;
-    } else {
-      mode = VFO;
-    } 
+    ModeSetting = (ModeSetting == ModeChoices - 1) ? (Mode)0 : (Mode)(ModeSetting + 1);
     displayDirty = true;
   }
 
@@ -430,19 +456,19 @@ void loop() {
 
   // Handle scanning.  If we are in VFO mode and scanning is enabled and the scan interval
   // has expired then step the VFO frequency.
-  if (mode == VFO && 
+  if (ModeSetting == VFO && 
       scanMode && 
       millis() > (lastScanStamp + scanDelayMs)) {
     // Record the time so that we can start another cycle
     lastScanStamp = millis();
     // Bump the frequency by the configured step
-    long step = stepMenu[stepIndex];
-    vfoFreq += step;
+    long step = Step[StepSetting];
+    DisplayFreq += step;
     // Look for wrap-around
-    if (vfoFreq > maxDisplayFreq) {
-      vfoFreq = minDisplayFreq;
+    if (DisplayFreq > MaxDisplayFreq) {
+      DisplayFreq = MinDisplayFreq;
     }
-    updateVFOFreq();
+    updateLOFreq();
     displayDirty = true;
   }
 
