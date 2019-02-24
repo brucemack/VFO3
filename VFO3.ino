3bfe98cd3ad0ff16389320b46047fbb3c8e1f8cd
Bruce MacKinnon <bruce@mackinnon.com>
2019-02-24 09:27:50 -0500

    Working with USB/LSB mode

--- VFO3.ino
+++ VFO3.ino
@@ -1,3 +1,6 @@
+// Control script for the Peppermint II SSB Tranceiver
+// Bruce MacKinnon KC1FSZ
+//
 #include <SPI.h>
 #include <Wire.h>
 #include <EEPROM.h>
@@ -14,25 +17,35 @@
 
 #define MAGIC_NUMBER 2727
 
-#define OLED_RESET 4
-Adafruit_SSD1306 display(OLED_RESET);
-
+Adafruit_SSD1306 display(128,64);
 Si5351 si5351;
 
+// Rotary encoder pins
 #define PIN_D2 2
 #define PIN_D3 3
 #define PIN_D4 4
+// Red command buttons
 #define COMMAND_BUTTON1_PIN 5
+#define COMMAND_BUTTON2_PIN 6
+// PTT controls 
+#define PTT_CONTROL_PIN 7
+#define PTT_BUTTON_PIN 8
+#define PTT_AUX_PIN 9
+// Analog sample input
+#define DETECTOR_INPUT_PIN A0
 
-enum Mode { VFO, VFO_OFFSET, BFO, CAL, VFO_POWER, BFO_POWER  };
-const char* modeTitles[] = { "VFO", "VFO+", "BFO", "CAL", "VFOPwr", "BFOPwr" };
+enum Mode { VFO, VFO_OFFSET, BFO, CAL, VFO_POWER, BFO_POWER, METER, MODE };
+const char* modeTitles[] = { "VFO", "VFO+", "BFO", "CAL", "VFOPwr", "BFOPwr", "Meter", "Mode" };
 Mode mode = VFO;
 
 const unsigned long stepMenu[] = { 500, 100, 10, 1, 1000000, 100000, 10000, 1000 };
 const char* stepMenuTitles[] = { "500 Hz", "100 Hz", "10 Hz", "1 Hz", "1 MHz", "100 kHz", "10 kHz", "1 kHz" };
 const uint8_t maxStepIndex = 7;
 
-// 40m band limitations
+enum Mode2 { LSB, USB };
+Mode2 mode2 = LSB;
+
+// 40m band limitations used for scanning (phone portion only)
 const unsigned long minDisplayFreq = 7125000L;
 const unsigned long maxDisplayFreq = 7300000L;
 
@@ -40,6 +53,8 @@ DebouncedSwitch2 db2(5L);
 DebouncedSwitch2 db3(5L);
 DebouncedSwitch2 db4(10L);
 DebouncedSwitch2 commandButton1(10L);
+DebouncedSwitch2 commandButton2(10L);
+DebouncedSwitch2 pttButton(20L);
 RotaryEncoder renc(&db2,&db3,100L);
 ClickDetector cd4(&db4);
 
@@ -50,14 +65,27 @@ long calPpm = 0;
 uint8_t stepIndex = 0;
 uint8_t vfoPower = 0;
 uint8_t bfoPower = 0;
+int analogSample = 0;
 
 // Scanning related.
-// This controls the mode: 0 means not scanning, +1 means scan up, -1 means scan down
 bool scanMode = false;
 // This is the last time we made a scan jump
 unsigned long lastScanStamp = 0;
 // This controls how fast we scan
 unsigned long scanDelayMs = 150;
+
+// Create an IntervalTimer object 
+IntervalTimer myTimer;
+
+int outTable[16];
+int outTablePtr = 0;
+
+void audioCycle() {
+  analogWrite(A14,outTable[outTablePtr++]);
+  if (outTablePtr == 16) {
+    outTablePtr = 0;  
+  }
+}
 
 unsigned long getMH(unsigned long f) {
   return f / 1000000L;
@@ -71,6 +99,95 @@ unsigned long getH(unsigned long f) {
   return f % 1000L;
 }
 
+void updateDisplay1() {
+
+  int startX = 10;
+  int y = 17;
+  char buf[4];
+  // Render frequency
+  unsigned long f = 0;
+  boolean neg = false;  
+
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  
+  if (mode == VFO) {
+    f = vfoFreq;
+    display.setCursor(startX,y);
+    display.print(getMH(f)); 
+    display.setCursor(startX + 30,y);
+    sprintf(buf,"%03lu",getKH(f));
+    display.print(buf);
+    display.setCursor(startX + 70,y);
+    sprintf(buf,"%03lu",getH(f));
+    display.print(buf);
+    // Step
+    display.setTextSize(0);
+    display.setCursor(startX,55);
+    display.print(stepMenuTitles[stepIndex]);
+  } else if (mode == VFO_OFFSET) {
+    f = vfoOffsetFreq;
+    display.setCursor(startX,y);
+    display.print(getMH(f)); 
+    display.setCursor(startX + 30,y);
+    sprintf(buf,"%03lu",getKH(f));
+    display.print(buf);
+    display.setCursor(startX + 70,y);
+    sprintf(buf,"%03lu",getH(f));
+    display.print(buf);
+    // Step
+    display.setTextSize(0);
+    display.setCursor(startX,55);
+    display.print(stepMenuTitles[stepIndex]);
+  } else if (mode == BFO) {
+    f = bfoFreq;
+    display.setCursor(startX,y);
+    display.print(getMH(f)); 
+    display.setCursor(startX + 30,y);
+    sprintf(buf,"%03lu",getKH(f));
+    display.print(buf);
+    display.setCursor(startX + 70,y);
+    sprintf(buf,"%03lu",getH(f));
+    display.print(buf);
+    // Step
+    display.setTextSize(0);
+    display.setCursor(startX,55);
+    display.print(stepMenuTitles[stepIndex]);
+  } else if (mode == CAL) {
+    f = abs(calPpm);
+    neg = (calPpm < 0);
+    // Sign
+    if (neg) {
+      display.drawLine(0,y+6,5,y+6,1);
+    }
+    display.setCursor(startX,y);
+    display.print(f); 
+    // Step
+    display.setTextSize(0);
+    display.setCursor(startX,55);
+    display.print(stepMenuTitles[stepIndex]);
+  } else if (mode == VFO_POWER) {
+    f = vfoPower;
+    display.setCursor(startX,y);
+    display.print(f); 
+  } else if (mode == BFO_POWER) {
+    f = bfoPower;
+    display.setCursor(startX,y);
+    display.print(f); 
+  } else if (mode == METER) {
+    f = analogSample;
+    display.setCursor(startX,y);
+    display.print(f); 
+  } else if (mode == MODE) {
+    display.setCursor(startX,y);
+    if (mode2 == LSB) {
+      display.print("LSB"); 
+    } else if (mode2 == USB) {
+      display.print("USB");       
+    }
+  }
+}
+
 void updateDisplay() {
 
   // Logo information and line
@@ -80,74 +197,26 @@ void updateDisplay() {
   display.println("KC1FSZ VFO3");
   display.drawLine(0,15,display.width(),15,WHITE);
   
-  int startX = 10;
-  int y = 17;
-
   // Mode
   int modeX = 85;
   display.setCursor(modeX,0);
   display.print(modeTitles[(int)mode]);
 
-  display.setTextSize(2);
-  display.setTextColor(WHITE);
-  char buf[4];
-
-  // Render frequency
-  unsigned long f = 0;
-  boolean neg = false;  
-  if (mode == VFO) {
-    f = vfoFreq;
-  } else if (mode == VFO_OFFSET) {
-    f = vfoOffsetFreq;
-  } else if (mode == BFO) {
-    f = bfoFreq;
-  } else if (mode == CAL) {
-    f = abs(calPpm);
-    neg = (calPpm < 0);
-  } else if (mode == VFO_POWER) {
-    f = vfoPower;
-  } else if (mode == BFO_POWER) {
-    f = bfoPower;
-  }
-
-  // Sign
-  if (neg) {
-    display.drawLine(0,y+6,5,y+6,1);
-  } else {
-    display.drawLine(0,y+6,5,y+6,0);
-  }
-
-  // Number
-  if (mode == VFO || mode == VFO_OFFSET || mode == BFO) {
-    display.setCursor(startX,y);
-    display.print(getMH(f)); 
-  
-    display.setCursor(startX + 30,y);
-    sprintf(buf,"%03lu",getKH(f));
-    display.print(buf);
-    
-    display.setCursor(startX + 70,y);
-    sprintf(buf,"%03lu",getH(f));
-    display.print(buf);
-  } else {
-    display.setCursor(startX,y);
-    display.print(f); 
-  }
-  
-  // Step
-  if (mode == VFO || mode == VFO_OFFSET || mode == BFO || mode == CAL) {
-    display.setTextSize(0);
-    display.setCursor(startX,55);
-    display.print(stepMenuTitles[stepIndex]);
-  }
+  updateDisplay1();
 }
 
 /**
  * Called to load frequencies into the Si5351
  */
 void updateVFOFreq() {
-  long f = vfoOffsetFreq - vfoFreq;
-  si5351.set_freq((unsigned long long)f * 100ULL,SI5351_CLK0);
+  long f;
+  if (mode2 == LSB) {
+    f = vfoOffsetFreq - vfoFreq;
+    si5351.set_freq((unsigned long long)f * 100ULL,SI5351_CLK0);
+  } else if (mode2 == USB) {
+    f = vfoOffsetFreq + vfoFreq;
+    si5351.set_freq((unsigned long long)f * 100ULL,SI5351_CLK0);    
+  }
 }
 
 void updateBFOFreq() {
@@ -168,16 +237,33 @@ void updateCal() {
 }
 
 void setup() {
-  
-  Serial.begin(9600);
+
+  //Serial.begin(9600);
   delay(500);
 
   pinMode(PIN_D2,INPUT_PULLUP);
   pinMode(PIN_D3,INPUT_PULLUP);
   pinMode(PIN_D4,INPUT_PULLUP);
   pinMode(COMMAND_BUTTON1_PIN,INPUT_PULLUP);
+  pinMode(COMMAND_BUTTON2_PIN,INPUT_PULLUP);
+  pinMode(PTT_BUTTON_PIN,INPUT_PULLUP);
+  pinMode(PTT_AUX_PIN,INPUT_PULLUP);
+  pinMode(PTT_CONTROL_PIN,OUTPUT);
+  pinMode(13,OUTPUT);
 
-  display.begin(SSD1306_SWITCHCAPVCC,SSD1306_I2C_ADDRESS);
+  digitalWrite(PTT_CONTROL_PIN,0);
+
+  // Diagnostic signal
+  delay(250);
+  digitalWrite(13,1);
+  delay(250);
+  digitalWrite(13,0);
+  delay(250);
+  digitalWrite(13,1);
+  delay(250);
+  digitalWrite(13,0);
+
+  display.begin(SSD1306_SWITCHCAPVCC,0x3c,true,true);
 
   // Si5351 initialization
   si5351.init(SI5351_CRYSTAL_LOAD_8PF,0,0);
@@ -215,12 +301,24 @@ void setup() {
   // Initial display render
   updateDisplay();
   display.display();
+
+  for (int i = 0; i < 16; i++) {
+    float rad = 2.0f * 3.14159f * ((float)i / 16.0f);
+    float a = sin(rad);
+    float b = a + 1.0f;
+    float c = (b / 2.0f) * 1024.0f;
+    outTable[i] = (int)c;
+  }
+
+  // Audio cycle 
+  myTimer.begin(audioCycle,200);
 }
 
 int count = 0;
 
 void loop() {
     
+  boolean displayDirty = false;
   int c2 = digitalRead(PIN_D2);
   int c3 = digitalRead(PIN_D3);
 
@@ -228,15 +326,21 @@ void loop() {
   db3.loadSample(c3 == 0);
   db4.loadSample(digitalRead(PIN_D4) == 0);
   commandButton1.loadSample(digitalRead(COMMAND_BUTTON1_PIN) == 0);
-  
+  commandButton2.loadSample(digitalRead(COMMAND_BUTTON2_PIN) == 0);
+  pttButton.loadSample(digitalRead(PTT_BUTTON_PIN) == 0);
+  int pttAux = digitalRead(PTT_AUX_PIN);
   long mult = renc.getIncrement();
   long clickDuration = cd4.getClickDuration();
+
+  int sample = analogRead(DETECTOR_INPUT_PIN);
   
-  boolean displayDirty = false;
+  if (sample != analogSample && mode == METER) {
+    displayDirty = true;
+  }
+  analogSample = sample;
 
   // Look for dial turning
   if (mult != 0) {
-    Serial.println(mult);
     // Immediately stop scanning
     scanMode = false;
     // Handle dial
@@ -265,6 +369,12 @@ void loop() {
         bfoPower = 0;
       }
       updateBFOPower();     
+    } else if (mode == MODE) {
+      if (mode2 == LSB) {
+        mode2 = USB;
+      } else if (mode2 == USB) {
+        mode2 = LSB;
+      }
     }
 
     displayDirty = true;
@@ -279,8 +389,16 @@ void loop() {
     EEPROM.write(20,stepIndex);
     EEPROM.write(21,vfoPower);
     EEPROM.write(22,bfoPower);
-  }
-  else if (clickDuration > 500) {
+  } else if (clickDuration > 0) {
+    if (++stepIndex > maxStepIndex) {
+      stepIndex = 0;
+    } 
+    displayDirty = true;
+  } else if (commandButton1.getState() && commandButton1.isEdge()) {
+    if (mode == VFO) {
+      scanMode = !scanMode;     
+    }
+  } else if (commandButton2.getState() && commandButton2.isEdge()) {
     if (mode == VFO) {
       mode = VFO_OFFSET;
     } else if (mode == VFO_OFFSET) {
@@ -291,24 +409,27 @@ void loop() {
       mode = VFO_POWER;
     } else if (mode == VFO_POWER) {
       mode = BFO_POWER;
+    } else if (mode == BFO_POWER) {
+      mode = METER;
+    } else if (mode == METER) {
+      mode = MODE;
     } else {
       mode = VFO;
     } 
     displayDirty = true;
-  } else if (clickDuration > 0) {
-    if (++stepIndex > maxStepIndex) {
-      stepIndex = 0;
-    } 
-    displayDirty = true;
-  } else if (commandButton1.getState() && commandButton1.isEdge()) {
-    if (mode == VFO) {
-      scanMode = !scanMode;     
-    }
+  }
+
+  // Deal with the PPT button
+  if (pttButton.getState() || pttAux == 0) {
+    digitalWrite(13,1);
+    digitalWrite(PTT_CONTROL_PIN,1);
+  } else {
+    digitalWrite(13,0);
+    digitalWrite(PTT_CONTROL_PIN,0);    
   }
 
   // Handle scanning.  If we are in VFO mode and scanning is enabled and the scan interval
   // has expired then step the VFO frequency.
-  //  
   if (mode == VFO && 
       scanMode && 
       millis() > (lastScanStamp + scanDelayMs)) {
